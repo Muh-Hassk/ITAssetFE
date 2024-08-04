10e78ee1ab75113b6107dd4596f64f8e88aadbb0
Muh-Hassk <Muh.alkhuzaee@gmail.com>
2024-08-04 07:04:18 +0300

    Final Project -- might need some Toasters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { AgGridModule } from 'ag-grid-angular';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,6 +13,13 @@ import { AssetManageComponent } from './asset-manage/asset-manage.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AssetGridComponent } from './asset-grid/asset-grid.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'; // For native date adapter
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,7 +36,18 @@ import { AssetGridComponent } from './asset-grid/asset-grid.component';
     BrowserAnimationsModule,
     HttpClientModule,
     HighchartsChartModule,
-    AgGridModule
+    AgGridModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    FormsModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/asset-manage/asset-manage.component.ts
+++ src/app/asset-manage/asset-manage.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DataServiceService } from '../Services/data-service.service';
 
 @Component({
   selector: 'app-asset-manage',
   templateUrl: './asset-manage.component.html',
   styleUrls: ['./asset-manage.component.css']
 })
-export class AssetManageComponent {
+export class AssetManageComponent implements OnInit {
+  @ViewChild('dialogContent') dialogContent!: TemplateRef<any>;
 
+  newAssetForm: FormGroup;
+  statusOptions: string[] = ['New', 'In Use', 'Damaged', 'Dispose'];
+
+  constructor(
+    private fb: FormBuilder,
+    private dataService: DataServiceService,
+    public dialog: MatDialog
+  ) {
+    // Initialize the form group
+    this.newAssetForm = this.fb.group({
+      brand: ['', Validators.required],
+      serialNumber: ['', Validators.required],
+      status: ['', Validators.required],
+      warrantyExpirationDate: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  openCreateAssetDialog(): void {
+    const dialogRef = this.dialog.open(this.dialogContent, {
+      width: '600px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+     
+    });
+  }
+
+  onSubmit(): void {
+    if (this.newAssetForm.valid) {
+      const newAsset = this.newAssetForm.value;
+      this.dataService.createAsset(newAsset)
+        this.dialog.closeAll();
+    }
+  }
 }
